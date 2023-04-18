568e85af9037f051c0b1cf1e1ac222bbadff4603
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-04-18 20:20:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+class API{
+    #secure;
+    url;
+    method;
+    constructor(url){
+        this.url=url;
+        if(url.includes("https"))
+        this.#secure=true;
+        else
+        this.#secure=false;
+        this.method="GET"
+    }
+    isSecure(){
+        return this.#secure;
+    }
+    updateUrl(url){
+        this.url=url
+        if(url.includes("https"))
+        this.#secure=true;
+        else
+        this.#secure=false;
+    }
+}
 
+module.exports = { API }
 
-module.exports = { API }
+ES6 Class
